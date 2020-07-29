a15cd33f377d6e3a8a02821b53cb75759032e896
闻冰 <wenbing.xwb@antfin.com>
2020-07-29 11:18:10 +0800

    fix: 修复解析 HTML URL问题

--- src/node2SketchSymbol.ts
+++ src/node2SketchSymbol.ts
@@ -1,14 +1,23 @@
 import puppeteer from 'puppeteer';
 import express from 'express';
 import { resolve } from 'path';
+import fs from 'fs';
 
 const hostname = '127.0.0.1'; //IP地址
-const port = 8973; //端口号
+
+// const port = (Math.random() * 10000).toFixed(0); //端口号
+const port = 6783; //端口号
 
 const baseURL = `http://${hostname}:${port}`;
 
 export const initNode2SketchSymbol = (filePath: string, url: string) => {
   const app = express();
+  const html = fs.readFileSync(filePath + '/index.html', 'utf8');
+
+  let temp = html.replace('href="/umi', 'href="umi');
+  temp = temp.replace('src="/umi', 'src="umi');
+
+  fs.writeFileSync(filePath + '/index.html', temp);
 
   app.use(url, express.static(filePath)); //指定静态文件目录
 
@@ -34,6 +43,7 @@ export const initNode2SketchSymbol = (filePath: string, url: string) => {
       );
       await browser.close();
 
+      fs.writeFileSync(filePath + '/index.html', html);
       return json;
     } catch (e) {
       await browser.close();
