8295010844707a317da69d86288496c913f7b02f
闻冰 <wenbing.xwb@antfin.com>
2020-07-31 12:56:53 +0800

    fix: fix Error

--- src/node2Symbol.ts
+++ src/node2Symbol.ts
@@ -1,6 +1,9 @@
 import {nodeToSketchSymbol} from "html2sketch";
 
 export function run(mainNode = document.body) {
+  if (!mainNode) {
+    throw Error('没有接收到元素,请检查输入')
+  }
   const page = nodeToSketchSymbol(mainNode);
 
   return page.toSketchJSON();
